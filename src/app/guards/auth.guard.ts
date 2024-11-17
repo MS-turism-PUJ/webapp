04d2605d648ae04236721f78ef36ad5e750a50d6
santicm23 <santicastrom@outlook.com>
2024-11-17 18:21:13 -0500

    adding role guard and saving credentials in local storage

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,9 +2,9 @@ import { CanActivateFn, Router } from '@angular/router'
 import { AuthService } from '../services/auth.service'
 import { inject } from '@angular/core'
 
-export const authGuardGuard: CanActivateFn = (route, state) => {
+export const authGuardGuard: CanActivateFn = async (route, state) => {
   const authService = inject(AuthService)
-  if (authService.isAuthenticated()) return true
+  if (await authService.isAuthenticated()) return true
 
   const router = inject(Router)
   router.navigate(['/login'])
