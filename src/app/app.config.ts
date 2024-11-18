28166eccf89d575bba4119f312d80b38c21397f6
santicm23 <santicastrom@outlook.com>
2024-11-17 22:01:00 -0500

    adding apollo client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule, provideHttpClient } from '@angular/common/http'; 
 import { routes } from './app.routes';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +17,15 @@ export const appConfig: ApplicationConfig = {
       GoogleMapsModule,
       FormsModule,
       HttpClientModule 
-    ),
+    ), provideHttpClient(), provideApollo(() => {
+      const httpLink = inject(HttpLink);
+
+      return {
+        link: httpLink.create({
+          uri: '/marketplace/graphql',
+        }),
+        cache: new InMemoryCache(),
+      };
+    }),
   ],
 };
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Apollo, gql } from 'apollo-angular';
 
 @Injectable({
     providedIn: 'root'
@@ -9,10 +10,20 @@ export class WeatherService {
     // URL del backend expuesto por WeatherController
     private apiUrl = 'http://localhost:8080/api/weather';
 
-    constructor(private http: HttpClient) { }
+    constructor(private apollo: Apollo) { }
 
-    getWeather(city: string): Observable<any> {
-        // Realizar la solicitud HTTP al backend con el parámetro city
-        return this.http.get(`${this.apiUrl}?city=${city}`);
+    async getWeather(city: string): Promise<any> {
+        return this.apollo.query({
+            query: gql`{
+                getWeather(city: $city) {
+                    city
+                    temperature
+                    weather
+                }
+            }`,
+            variables: {
+                city: city
+            }
+        }).toPromise();
     }
 }
