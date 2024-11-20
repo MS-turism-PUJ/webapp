fbc34a6df51dfcf2759dc1d51cfafecd51291628
Kose117 <josemrodriguez@javeriana.edu.co>
2024-11-20 14:36:44 -0500

    fix

--- src/app/config/environment/urls.ts
+++ src/app/config/environment/urls.ts
@@ -1,6 +1,2 @@
-import { config } from 'dotenv'
-config({
-  path: '../.env'
-});
 
-export const API_URL = process.env['API_URL'] || 'http://localhost:8080';
+export const API_URL =  'http://localhost:8080';
